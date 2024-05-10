bbbf42a8c6f859cea657f6592d346dc42a28bda5
Szymon Kościelski <nomyze@proton.me>
2024-05-10 12:15:56 +0200

    clean

--- Models/Visit.cs
+++
@@ -1,8 +0,0 @@
-namespace apbd4.Models;
-
-public class Visit {
-    public int animal_id {get; set;}
-    public DateOnly date {get; set;}
-    public string? description {get; set;}
-    public double price {get; set;}
-}
